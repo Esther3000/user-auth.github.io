99105d3af38bd56aaed67b0024eb0ff986e494ca
Esther3000 <esthermaina3000@gmail.com>
2023-06-16 19:17:11 +0300

    fixing render errors

--- app.js
+++ app.js
@@ -9,17 +9,11 @@ const auth = require("./auth");
 // Execute database connection
 dbConnect();
 
-// Middleware
+// Allow cross-origin requests
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use(express.json());
